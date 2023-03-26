628a15f1d2a81fdac8d041414a5622d22212ec9e
Israel <israelias94@gmail.com>
2023-03-26 03:59:17 -0400

    feat: filters by entity type

---
+++ src/app/components/SearchFilters.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import Checkbox from "@mui/material/Checkbox";
+import { useEffect, useState } from "react";
+
+
+export enum EntityType {
+  COLLECTION = "collection.collection",
+  ARTICLE = "curation.article",
+  AUDIO = "curation.audio",
+  BOOK = "curation.book",
+  HIGHLIGHT = "contribution.highlight",
+  IMAGE = "curation.image",
+  TEXT = "curation.text",
+  TWEET = "curation.tweet",
+  VIDEO = "curation.video",
+  LINK = "curation.link",
+}
+
+export interface SearchFiltersProps {
+  handleSelect: (event: any) => void;
+  filters: EntityType[];
+}
+
+export default function SearchFilters(props: SearchFiltersProps) {
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+        <FormLabel component="legend">Choose Filter</FormLabel>
+        <FormGroup>
+          {Object.values(EntityType).map((entity) => (
+            <FormControlLabel
+              key={entity}
+              control={
+                <Checkbox
+                  checked={props.filters.includes(entity)}
+                  onChange={props.handleSelect}
+                  name={entity}
+                />
+              }
+              label={entity}
+            />
+          ))}
+        </FormGroup>
+        <FormHelperText>Please avoid me to translate entity types 🙃</FormHelperText>
+      </FormControl>
+    </Box>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,140 +1,208 @@
 "use client";
 
-import { Box, Container, Dialog, DialogContent, DialogContentText, FormControl, Grid, InputAdornment, InputLabel, OutlinedInput, Typography } from '@mui/material';
-import { JsonViewer } from '@textea/json-viewer';
-import { Inter } from 'next/font/google';
-import { useState } from 'react';
-import styles from './page.module.css';
-const inter = Inter({ subsets: ['latin'] })
+import {
+  Box,
+  Container,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  FormControl,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Typography,
+} from "@mui/material";
+import { JsonViewer } from "@textea/json-viewer";
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import SearchFilters, { EntityType } from "./components/SearchFilters";
+import styles from "./page.module.css";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [data, setData] = useState([])
-  const [isLoading, setLoading] = useState(false)
+  const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
   const [openTokensModal, setOpenTokensModal] = useState(false);
   const [fieldToken, setFieldToken] = useState(null);
+  const [filters, setFilters] = useState<EntityType[]>([]);
 
+  useEffect(() => {
+    let controller = new AbortController();
+    let signal = controller.signal;
 
-  const handleSearch = async (event: any) => {
-    if (event.key === 'Enter' && searchTerm !== '') {
-      setLoading(true)
-      fetch(`${process.env.NEXT_PUBLIC_SUBLIME_HOST}/api/v2/feed/search/?search=${searchTerm}`)
+    if (filters.length > 0 || searchTerm != "") {
+      setLoading(true);
+      const filterParams = filters
+        .map((filter) => `entity_type=${filter}`)
+        .join("&");
+
+      fetch(
+        `${process.env.NEXT_PUBLIC_SUBLIME_HOST}/api/v2/feed/search/?search=${searchTerm}&${filterParams}`,
+        { signal }
+      )
         .then((res) => res.json())
         .then((data) => {
-          setData(data.results)
-          setLoading(false)
+          setData(data.results);
+          setLoading(false);
         })
+        .catch((error) => {
+          if (error.name === "AbortError") {
+            console.log("Fetch aborted");
+          } else {
+            console.log(error);
+          }
+        });
+
+      return () => {
+        controller.abort();
+      };
     }
-  }
+  }, [filters, searchTerm]);
+
+  const handleSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+
+    if (checked) {
+      setFilters([...filters, name as EntityType]);
+    } else {
+      setFilters(filters.filter((entity) => entity !== name));
+    }
+  };
 
   return (
     <main className={styles.main}>
-      <Container>
-        <Box sx={{ display: 'flex' }}>
+      <Container fixed>
+        <Box sx={{ display: "flex" }}>
           <FormControl fullWidth sx={{ m: 1 }}>
             <InputLabel htmlFor="outlined-adornment-amount">Buscar</InputLabel>
             <OutlinedInput
               id="outlined-adornment-amount"
               value={searchTerm}
               onChange={(event) => setSearchTerm(event.target.value)}
-              onKeyDown={handleSearch}
-              startAdornment={<InputAdornment position="start">&#707;</InputAdornment>}
+              startAdornment={
+                <InputAdornment position="start">&#707;</InputAdornment>
+              }
               label="Amount"
             />
           </FormControl>
         </Box>
-        <Box>
-          <Dialog
-            open={openTokensModal}
-            onClose={() => setOpenTokensModal(false)}
-            scroll='body'
-            maxWidth='md'
-            fullWidth
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <DialogContent dividers={false}>
-              <DialogContentText>
-                <div>
-                  <JsonViewer value={fieldToken} />
-                </div>
-              </DialogContentText>
-            </DialogContent>
-          </Dialog>
-          {isLoading ? <p>Loading...</p> : <></>}
-          {!data ? <p>No data...</p> : <></>}
-          {!isLoading && data &&
-            data.map((result: any) => (
-              <div key={result.uuid}>
-                <Grid container m={2} spacing={2}>
-                  <Grid item xs={10}>
-                    <Box onClick={() => {
-                      setOpenTokensModal(true);
-                      setFieldToken(result.tokens['name'])
-                    }}>
-                      <Typography variant='h6'>{result.name}</Typography>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Box onClick={() => {
-                      setOpenTokensModal(true);
-                      setFieldToken(JSON.parse(result.explanation));
-                    }}>
-                      <Typography variant='body1' align='right'>{Number.parseFloat(result.score).toFixed(2)}</Typography>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={9}>
-                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                      <Typography variant='caption'>
-                        <Box sx={{ textTransform: 'lowercase' }}>
-                          {result.tagline}
-                        </Box>
-                      </Typography>
-                      <Box mx={1}></Box>
-                      <Typography variant='caption'>by</Typography>
-                      <Box mx={1}></Box>
-                      <Box onClick={() => {
-                        setOpenTokensModal(true);
-                        setFieldToken(result.tokens['authors'])
-                      }}>
-                        <Typography variant='caption'>{result.authors.join(' ')}</Typography>
+        <Dialog
+          open={openTokensModal}
+          onClose={() => setOpenTokensModal(false)}
+          scroll="body"
+          maxWidth="md"
+          fullWidth
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <DialogContent dividers={false}>
+            <DialogContentText>
+              <div>
+                <JsonViewer value={fieldToken} />
+              </div>
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+        <Grid container>
+          <Grid item xs={3}>
+            <SearchFilters handleSelect={handleSelect} filters={filters} />
+          </Grid>
+          <Grid item xs={9}>
+            {isLoading ? <p>Loading...</p> : <></>}
+            {!data ? <p>No data...</p> : <></>}
+            {!isLoading &&
+              data &&
+              data.map((result: any) => (
+                <div key={result.uuid}>
+                  <Grid container m={2} spacing={2}>
+                    <Grid item xs={10}>
+                      <Box
+                        onClick={() => {
+                          setOpenTokensModal(true);
+                          setFieldToken(result.tokens["name"]);
+                        }}
+                      >
+                        <Typography variant="h6">{result.name}</Typography>
                       </Box>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={9}>
-                    <Box onClick={() => {
-                      setOpenTokensModal(true);
-                      setFieldToken(result.tokens['urls']);
-
-                    }}>
-                      <Typography>{result.urls}</Typography>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={9}>
-                    <Box onClick={() => {
-                      setOpenTokensModal(true);
-                      setFieldToken(result.tokens['description']);
-                    }}>
-                      {
-                        result.embed.html ?
-                          <div className="embed-tweet" dangerouslySetInnerHTML={{ __html: result.embed.html }} /> :
-                          <Typography variant='body2'>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Box
+                        onClick={() => {
+                          setOpenTokensModal(true);
+                          setFieldToken(JSON.parse(result.explanation));
+                        }}
+                      >
+                        <Typography variant="body1" align="right">
+                          {Number.parseFloat(result.score).toFixed(2)}
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                        <Typography variant="caption">
+                          <Box sx={{ textTransform: "lowercase" }}>
+                            {result.tagline}
+                          </Box>
+                        </Typography>
+                        <Box mx={1}></Box>
+                        <Typography variant="caption">by</Typography>
+                        <Box mx={1}></Box>
+                        <Box
+                          onClick={() => {
+                            setOpenTokensModal(true);
+                            setFieldToken(result.tokens["authors"]);
+                          }}
+                        >
+                          <Typography variant="caption">
+                            {result.authors.join(" ")}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <Box
+                        onClick={() => {
+                          setOpenTokensModal(true);
+                          setFieldToken(result.tokens["urls"]);
+                        }}
+                      >
+                        <Typography>{result.urls}</Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <Box
+                        onClick={() => {
+                          setOpenTokensModal(true);
+                          setFieldToken(result.tokens["description"]);
+                        }}
+                      >
+                        {result.embed.html ? (
+                          <div
+                            className="embed-tweet"
+                            dangerouslySetInnerHTML={{
+                              __html: result.embed.html,
+                            }}
+                          />
+                        ) : (
+                          <Typography variant="body2">
                             {result.description ? result.description : null}
                             {result.about ? result.about : null}
                             {result.bio ? result.bio : null}
                             {result.html ? result.html : null}
                             {result.text ? result.text : null}
                           </Typography>
-                      }
-                    </Box>
+                        )}
+                      </Box>
+                    </Grid>
                   </Grid>
-                </Grid>
-                <hr />
-              </div>
-            ))
-          }
-        </Box>
+                  <hr />
+                </div>
+              ))}
+          </Grid>
+        </Grid>
       </Container>
     </main>
-  )
+  );
 }
